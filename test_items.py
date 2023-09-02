3d39279c97a75d4f95e7367255b703739dafba67
Лидия <121150862+Tglids@users.noreply.github.com>
2023-09-02 16:28:54 +0300

    Add files via upload
    Добавлен time.sleep() :D

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
+import time
 from selenium.webdriver.common.by import By
 
 def test_choose_your_language(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
+    time.sleep(30)
     browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
 
 
